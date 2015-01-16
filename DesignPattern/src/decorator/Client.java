195e9f88478a1f1667f24fe31c0ca6bc4b8c70ab
suffermore <cjclancelot@gmail.com>
2015-01-16 01:09:03 -0500

    commit decorator finally

--- DesignPattern/src/decorator/Client.java
+++ DesignPattern/src/decorator/Client.java
@@ -3,16 +3,25 @@ package decorator;
 public class Client {
 	public static void main(String args[]) {
 		
+		//component A: Nokia
 		AbstractCellPhone phone = new NokiaPhone();
 		System.out.println(phone.CallNumber());
 		System.out.println(phone.SendMessage());
 		
-		DecoratorGPS gps = new DecoratorGPS(phone);
-		System.out.println(gps.CallNumber());
-		System.out.println(gps.SendMessage());
+		DecoratorGPS gpsNokia = new DecoratorGPS(phone);
+		System.out.println(gpsNokia.CallNumber());
+		System.out.println(gpsNokia.SendMessage());
 		
-		DecoratorBluetooth bluetooth = new DecoratorBluetooth(phone);
-		System.out.println(bluetooth.CallNumber());
-		System.out.println(bluetooth.SendMessage());
+		DecoratorBluetooth bluetoothNokia = new DecoratorBluetooth(phone);
+		System.out.println(bluetoothNokia.CallNumber());
+		System.out.println(bluetoothNokia.SendMessage());
+		
+		//component B: iphone
+		AbstractCellPhone iphone = new IPhone();
+		DecoratorGPS gpsIphone = new DecoratorGPS(iphone);
+		System.out.println("\n" + gpsIphone.CallNumber());
+		
+		DecoratorBluetooth bluetoothIphone = new DecoratorBluetooth(phone);
+		System.out.println(bluetoothIphone.SendMessage());
 	}
 }
