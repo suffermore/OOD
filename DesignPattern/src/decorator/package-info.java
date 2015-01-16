35ff9f53583fdf0ca7b91a34ce3c1540318eebf2
suffermore <cjclancelot@gmail.com>
2015-01-16 13:04:11 -0500

    commit delete

--- DesignPattern/src/decorator/package-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * @author SufferMore
- * 
- * In software engineering, the singleton pattern is a design pattern that restricts the 
- * instantiation of a class to one object. This is useful when exactly one object is needed 
- * to coordinate actions across the system.
- * 
- */
-package decorator;
