18b36501ad4f39834ea0caf0a0996c5bf41c542f
suffermore <cjclancelot@gmail.com>
2015-02-17 19:45:26 -0500

    commit

--- DesignPattern/src/template/ConcreteMethodA.java
+++ DesignPattern/src/template/ConcreteMethodA.java
@@ -5,8 +5,9 @@ class ConcreteMethodA extends AbstractMethod{
 	protected void method1() {
 		System.out.println("subclass A £ºmethod1() ...");
 	} 
-	
+
 	protected void method2() {
 	    System.out.println("subclass A £ºmethod2() ..."); 
-	} 
+	}
+	
 }
